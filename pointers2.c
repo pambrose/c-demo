c49b8d26e8d7e8c508708551ce576d7b8535be6a
Paul Ambrose <pambrose@mac.com>
2022-07-18 14:22:19 -0700

    Update content

--- pointers2.c
+++ pointers2.c
@@ -4,6 +4,7 @@
 void pointers2() {
     int v[] = {10, 20, 30, 40, 50};
     int *p = v;
+
     printf("v = %p, p = %p\n", v, p);
     printf("v[0] = %d, *p = %d\n", v[0], *p);
     printf("v[1] = %d, *(p+1) = %d\n", v[1], *(p + 1));
