8b43c034d27832ad1b6c2e55ea9aba26e98080da
Paul Ambrose <pambrose@mac.com>
2022-07-18 12:49:02 -0700

    Update content

--- basic_types.c
+++ basic_types.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "basic_types.h"
 
 void basic_types() {
-    short int s = -32768;
     int i = 5;
+    short s = -32;
     long int l = 100;
     double d = 5.0;
     float f = 9.0;
+    unsigned u = 5;
     char c = 'a';
 }
