4ae73f9ea23465acddfb523fb28e17699d2c29d5
Paul Ambrose <pambrose@mac.com>
2022-07-18 13:54:05 -0700

    Update content

--- arrays2.c
+++ arrays2.c
@@ -4,7 +4,8 @@
 void arrays2() {
     // Allocate and assign an array of 3 integers
     int b[3] = {100, 200, 300};
-    printf("Size of b = %ld, size of int = %ld\n", sizeof(b), sizeof(int));
+    printf("sizeof(b) = %li, sizeof(int) = %li\n",
+           sizeof(b), sizeof(int));
     int len = sizeof(b) / sizeof(int);
     for (int i = 0; i < len; i++)
         printf("b[%d] = %d\n", i, b[i]);
