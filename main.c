db20649dc301bf78110d33d74ae6e460071a19c5
Paul Ambrose <pambrose@mac.com>
2022-07-19 10:08:18 -0700

    Update content

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include "pointers.h"
 #include "strings.h"
 
-int main(int argc, char *argv[]) {
-    //basic_types();
+int main() {
+    basic_types();
     //arrays1();
     //arrays2();
     //pointers1();
     //pointers2();
     //strings1();
     //strings2();
-    strings3();
+    //strings3();
     return 0;
 }
