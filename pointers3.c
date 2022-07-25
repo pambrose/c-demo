a4c2c8c80189d7f60eb503ebb213c76868670b3d
Paul Ambrose <pambrose@mac.com>
2022-07-25 00:10:45 -0700

    Update content

--- pointers3.c
+++ pointers3.c
@@ -6,12 +6,10 @@ void pointers3() {
     int *p = v;
 
     printf("v = %p, p = %p\n", v, p);
-
     printf("v[0] = %d, *v = %d, *p = %d\n",
            v[0], *v, *p);
     printf("v[1] = %d, *(v + 1) = %d, *(p + 1) = %d\n",
            v[1], *(v + 1), *(p + 1));
-
     for (int i = 2; i < 3; i++)
         printf("v[%d] = %d, *(v + %d) = %d, *(p + %d) = %d\n",
                i, v[i], i, *(v + i), i, *(p + i));
